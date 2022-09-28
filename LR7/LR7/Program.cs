3a7083e1ec6f0de99c9036319e01cfd39ea18bfe
infstka <malder100@tut.by>
2022-09-28 23:37:36 +0300

    add all LRs on MCS

---
+++ LR7/LR7/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using static ConsoleApp1.BS;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---------- Получаем необходимые данные и заполняем ими объекты БС
+            Console.Write("Введите количество БС : ");
+            int amountOfBS = int.Parse(Console.ReadLine());
+            Console.Write("Введите координационное расстояние : ");
+            int radius = int.Parse(Console.ReadLine());
+
+            // массив для хранения объектов станций
+            BS[] bs = new BS[amountOfBS];
+
+            //for (int i = 0; i < amountOfBS; i++)
+            //{
+            //    bs[i] = new BS();
+
+            //    // номер БС
+            //    Console.Write("\nВведите номер БС : ");
+            //    bs[i].Number = int.Parse(Console.ReadLine());
+
+            //    // координаты БС
+
+
+            //    Console.Write("Введите координаты x, y (Пример \"5 1\", где 5 по x, 1 по y) : ");
+            //    string coordsArray = Console.ReadLine();
+            //    string[] coords = coordsArray.Split(' '); // массив координат (x, y)
+            //    bs[i].CoordinatesXY = new BS.Coordinate(int.Parse(coords[0]), int.Parse(coords[1]));
+
+            //    // БС, работающие на той же частоте
+            //    Console.Write("Введите номера БС, которые работают на той же частоте, что и данная (Пример: 1 4 5) : ");
+            //    string sameFrequencyBSArray = Console.ReadLine();
+            //    string[] sameFrequencyBS = sameFrequencyBSArray.Split(' ');
+
+            //    // так как в классе мы храним массив БС на той же частоте, то определяем новый массив для заполнения
+            //    // номерами базовых станций той же частоты
+            //    bs[i].ArrayOfFrequencySameBS = new int[sameFrequencyBS.Length];
+            //    // заполняем массив номерами БС
+            //    for (int a = 0; a < sameFrequencyBS.Length; a++)
+            //    {
+            //        bs[i].ArrayOfFrequencySameBS[a] = int.Parse(sameFrequencyBS[a]);
+            //    }
+            //}
+
+            bs[0] = new BS(1, new Coordinate(0, 0), new int[] { 4 });
+            bs[1] = new BS(2, new Coordinate(2, 9), new int[] { 3, 5 });
+            bs[2] = new BS(3, new Coordinate(3, 3), new int[] { 2, 5 });
+            bs[3] = new BS(4, new Coordinate(-3, 5), new int[] { 1 });
+            bs[4] = new BS(5, new Coordinate(-5, 3), new int[] { 2, 3 });
+            bs[5] = new BS(6, new Coordinate(8, 9), new int[] { 7, 8 });
+            bs[6] = new BS(7, new Coordinate(-4, 9), new int[] { 6, 8 });
+            bs[7] = new BS(8, new Coordinate(2, 6), new int[] { 6, 7 });
+
+            Console.WriteLine("\nВывод введенных данных :");
+            for (int i = 0; i < amountOfBS; i++)
+                Console.WriteLine(bs[i].ToString());
+
+            // ---------- Построение графа связи с помощью координационых колец
+            // создаем матрицу смежности, по умолчанию она заполнится нулями
+            int[,] matrix = new int[amountOfBS, amountOfBS];
+
+            // i - индекс БС в массиве bs объектов базовых станций
+            for (int i = 0; i < amountOfBS; i++)
+            {
+                // a = индекс номера БС, которая записана в массив базовых станций той же частоты, что рассматриваемая
+                for (int a = 0; a < bs[i].ArrayOfFrequencySameBS.Length; a++)
+                {
+                    // расчет дистанции между рассматриваемой БС (bs[i]) и БС той же частоты (bs[bs[i].ArrayOfFrequencySameBS[a] - 1])
+                    // bs[i].ArrayOfFrequencySameBS[a] - 1 => 
+                    // bs[i] - берем рассматриваемую БС, 
+                    // ArrayOfFrequencySameBS[a] - 1 - берем номер БС из массива базовых станций той же частоты и отнимаем 1 
+                    // для получения индекса данной БС в массиве объектов базовых станций (bs)
+                    double dist = BS.Distance(bs[i].CoordinatesXY, bs[bs[i].ArrayOfFrequencySameBS[a] - 1].CoordinatesXY);
+
+                    if (dist < radius) // если Координационное расстояние  больше расстояние то есть пересечение и в матрицу записывается 1
+                        matrix[i, bs[bs[i].ArrayOfFrequencySameBS[a] - 1].Number - 1] = 1;
+                }
+            }
+
+            // ---------- Выводим матрицу смежности полученного графа
+            Console.Write("\n\nГраф:\n\n\t  ");
+            for (int i = 1; i <= amountOfBS; i++)
+                Console.Write(i + " ");
+            Console.WriteLine();
+
+            for (int row = 0; row < amountOfBS; row++)
+            {
+                Console.Write("\t" + (row + 1).ToString() + " "); // номер строки матрицы
+
+                for (int column = 0; column < amountOfBS; column++)
+                {
+                    Console.Write(matrix[row, column] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class BS
+    {
+        public int Number { get; set; }
+        public Coordinate CoordinatesXY { get; set; }
+        public int[] ArrayOfFrequencySameBS;
+
+        public BS(int number, Coordinate cord, int[] neib)
+        {
+            Number = number;
+            CoordinatesXY = cord;
+            ArrayOfFrequencySameBS = neib;
+        }
+
+        public BS()
+        {
+        }
+
+        public override string ToString()
+        {
+            string coordsArray = "";
+            for (int i = 0; i < this.ArrayOfFrequencySameBS.Length; i++)
+            {
+                coordsArray += ArrayOfFrequencySameBS[i] + " ";
+            }
+            return
+                "Номер: " + this.Number +
+                ", Координаты: (X:" + this.CoordinatesXY.X + ",Y:" + this.CoordinatesXY.Y +
+                "), на одной частоте с " + coordsArray;
+        }
+
+        public static double Distance(Coordinate A, Coordinate B)
+        {
+            // высчитываем расстояние между станциями как гипотенузу по теореме Пифагора
+            return Math.Sqrt(Math.Pow((B.X - A.X), 2) + Math.Pow((B.Y - A.Y), 2));
+        }
+
+        internal class Coordinate
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Coordinate(int _x, int _y)
+            {
+                X = _x;
+                Y = _y;
+            }
+        }
+    }
+}
---
+++ LR8/SMS8/SMS8/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SMS8
+{
+    internal class Program
+    { 
+        static class Task1 {
+        public static void task1(string input)
+        {
+            string mask = RandMask("01234567");
+            Console.WriteLine($"Маска - {mask}");
+
+            string afterInput = InterLeaving(input, mask);
+            Console.WriteLine($"Строка после перемежения - {afterInput}");
+            string afterErrors = ErrorsGenerator(afterInput, 3, 9);
+            Console.WriteLine($"Строка после воздействия ошибок - {afterErrors}");
+            string afterAntiInput = AntiInterLeaving(afterErrors, mask);
+            Console.WriteLine($"Строка после восстановления - {afterAntiInput}");
+            LessErorrs(afterInput, mask, 3);
+        }
+
+        static string RandMask(string str)
+        {
+            List<char> randArr = new List<char>();
+            Random random = new Random();
+            while (randArr.Count != str.Length)
+            {
+                char rand = random.Next(0, str.Length).ToString()[0];
+                if (!randArr.Contains(rand))
+                    randArr.Add(rand);
+            }
+            return new string(randArr.ToArray());
+        }
+
+        static string InterLeaving(string str, string mask) // Перемежение
+        {
+            string output = "";
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (char m in mask)
+                {
+                    output += str.Substring(i * 8, 8)[(int)Char.GetNumericValue(m)];
+                }
+            }
+            return output;
+        }
+
+        static string AntiInterLeaving(string str, string mask)
+        {
+            char[] antiArr = new char[32];
+            for (int i = 0; i < 4; i++)
+            {
+                foreach (char m in mask)
+                {
+                    antiArr[(int)Char.GetNumericValue(m) + 8 * i] = str.Substring(i * 8, 8)[mask.IndexOf(m)];
+                }
+            }
+            return new string(antiArr);
+        }
+
+        static string ErrorsGenerator(string str, int variant, int length)
+        {
+            char[] errors = str.ToCharArray();
+            string final = "";
+
+            for (int i = variant * 2; i < variant * 2 + length; i++)
+            {
+                errors[i] = ' ';
+            }
+
+            for (int i = 0; i < errors.Length; i++)
+                final += errors[i];
+
+            return final;
+        }
+
+        static string LessErorrs(string before, string mask, int variant)
+        {
+            Regex reg = new Regex(@"[а-яА-Я]\s\s[а-яА-Я|_*|]");
+            int losing_bits = 8;
+            string after_losing = before;
+            for (int i = 8; i != 0; i--)
+            {
+                after_losing = AntiInterLeaving(ErrorsGenerator(before, variant, i), mask);
+                Console.WriteLine(after_losing);
+                losing_bits = i;
+                if (reg.Match(after_losing).Success)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\nВыходная строка: " + after_losing);
+            Console.WriteLine("Число потерянных бит: " + losing_bits);
+            return after_losing;
+        }
+    }
+
+        static class Task2 {
+        public static void task2(string str)
+        {
+            string[] firstlvl = FirstLevel(str);
+            Console.WriteLine("Первый уровень перемежения:");
+            foreach (string el in firstlvl)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine("Второй уровень перемежения:");
+            string[] secondlvl = SecondLevel(firstlvl);
+            foreach (string el in secondlvl)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine("После потери 11 бит:");
+            string[] afterErrors = Errors(secondlvl, 4);
+            foreach (string el in afterErrors)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine("Первоначальная строка");
+            Console.WriteLine(InitialString(afterErrors));
+        }
+
+        static string[] FirstLevel(string str)
+        {
+            string[] sublvl = new string[8];
+            for(int i = 0; i < 8; i++)
+            {
+                sublvl[i] = str.Substring(i * 4, 4);
+            }
+            string[] output = new string[4];
+            for (int i = 0; i < 4; i++)
+            {
+                output[i] = $"{i + 1}{i + 1}{i + 1}{sublvl[i*2]}--{sublvl[i*2+1]}{i + 1}{i + 1}{i + 1}";
+            }
+            return output;
+        }
+        static string[] SecondLevel(string[] str)
+        {
+            string[] output = new string[4];
+            output[0] = $"{str[0]}--{str[3]}";
+            for (int i = 0; i < str[0].Length; i++)
+                output[0] += ' ';
+            for (int i = 1; i < 4; i++)
+            {
+                output[i] = $"{str[i]}--{str[i-1]}";
+            }
+            return output;
+        }
+
+        static string[] Errors(string[] str, int variant)
+        {
+            string[] output = new string[4];
+            string temp = "";
+            foreach (string i in str)
+            {
+                temp += i;
+            }
+            temp = temp.Substring(0, temp.Length / 4 - 4) + temp.Substring(temp.Length / 4 + 12);
+            char[] errors = temp.ToCharArray();
+            for (int i = variant * 2; i < variant * 2 + 11; i++)
+            {
+                errors[i] = ' ';
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                output[i] = new string(errors).Substring(errors.Length/4 * i, errors.Length / 4);
+            }
+            return output;
+        }
+
+        static string InitialString(string[] str)
+        { 
+            string[] temp = {str[1], str[2], str[3] };
+            string tempStr = "";
+            string output = "";
+            foreach (string i in temp)
+            {
+                tempStr += i;
+            }
+            tempStr = tempStr.Substring(16);
+            tempStr = tempStr.Substring(0, tempStr.Length - 16);
+            temp = tempStr.Split('-');
+            tempStr = "";
+            foreach (string i in temp)
+            {
+                tempStr += i;
+            }
+            return tempStr.Substring(3, 8) + tempStr.Substring(31, 8) + tempStr.Substring(17, 8) + tempStr.Substring(45, 8);
+        }
+    }
+        
+        public static void Main(string[] args)
+        {
+            string input = "БобровичГлебСергеевич_группа7___";
+
+
+            Console.WriteLine($"<- Задание 1 ->\n" +
+                              $"Исходная строка - {input}");
+            Task1.task1(input);
+            Console.ReadKey();
+            Console.Clear();
+            
+            Console.WriteLine($"<- Задание 2 ->" +
+                              $"\nИсходная строка - {input}");
+            Task2.task2(input);
+        }
+    }
+}
